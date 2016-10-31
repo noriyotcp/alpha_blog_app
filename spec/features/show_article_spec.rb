3efaba83beb23b05475a5e42d62ff26245b89afb
Noriyo Akita <noriyo.akita@gmail.com>
2016-10-31 12:27:16 +0000

    Add show_article_spec feature test

---
+++ spec/features/show_article_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "ShowArticle", type: :feature do
+  before do
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.")
+  end
+
+  scenario "A user shows an article" do
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content @article.title
+    expect(page).to have_content @article.body
+    expect(current_path).to eq article_path(@article)
+  end
+end
