e1d565e167b880d77e07b2c339f45e2371edd562
Noriyo Akita <noriyo.akita@gmail.com>
2016-10-31 12:27:54 +0000

    Display an article on show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all.order(created_at: :desc)
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
