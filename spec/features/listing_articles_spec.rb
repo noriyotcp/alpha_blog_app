2a311be8e256b355f2de06592a720b6babec9a21
Noriyo Akita <noriyo.akita@gmail.com>
2016-11-02 08:21:01 +0000

    [listing_articles_spec] Add a scenario when the user does not sign in and refactor

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -1,37 +1,54 @@
 require 'rails_helper'
 
 RSpec.feature "ListingArticles", type: :feature do
-  before do
-    login
-    @article1 = Article.create(title: "The first article", body: "Body of 1st article", user: @user)
-    @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: @user)
+  let!(:user) { User.create!(email: "john@example.com", password: "password") }
+  let!(:article1) { Article.create(title: "The first article", body: "Body of 1st article", user: user) }
+  let!(:article2) { Article.create(title: "The second article", body: "Body of 2nd article", user: user) }
+
+  feature 'when the user does not sign in' do
+    scenario "articles are shown but New Article link is not" do
+      visit "/"
+
+      expect(page).to have_content(article1.title)
+      expect(page).to have_content(article1.body)
+      expect(page).to have_content(article2.title)
+      expect(page).to have_content(article2.body)
+      expect(page).to have_link(article1.title)
+      expect(page).to have_link(article2.title)
+      expect(page).not_to have_link 'New Article'
+    end
   end
 
-  scenario "A user lists all articles" do
-    visit "/"
+  feature 'when the user signs in' do
+    scenario "A user lists all articles" do
+      login_as(user)
+      visit "/"
 
-    expect(page).to have_content(@article1.title)
-    expect(page).to have_content(@article1.body)
-    expect(page).to have_content(@article2.title)
-    expect(page).to have_content(@article2.body)
-    expect(page).to have_link(@article1.title)
-    expect(page).to have_link(@article2.title)
+      expect(page).to have_content(article1.title)
+      expect(page).to have_content(article1.body)
+      expect(page).to have_content(article2.title)
+      expect(page).to have_content(article2.body)
+      expect(page).to have_link(article1.title)
+      expect(page).to have_link(article2.title)
+    end
   end
 
-  scenario "A user has no articles" do
-    Article.delete_all
+  feature "A user has no articles" do
+    scenario "Display No Articles Created" do
+      Article.delete_all
 
-    visit "/"
+      visit "/"
 
-    expect(page).not_to have_content(@article1.title)
-    expect(page).not_to have_content(@article1.body)
-    expect(page).not_to have_content(@article2.title)
-    expect(page).not_to have_content(@article2.body)
-    expect(page).not_to have_link(@article1.title)
-    expect(page).not_to have_link(@article2.title)
+      expect(page).not_to have_content(article1.title)
+      expect(page).not_to have_content(article1.body)
+      expect(page).not_to have_content(article2.title)
+      expect(page).not_to have_content(article2.body)
+      expect(page).not_to have_link(article1.title)
+      expect(page).not_to have_link(article2.title)
 
-    within "h1#no-articles" do
-      expect(page).to have_content "No Articles Created"
+      within "h1#no-articles" do
+        expect(page).to have_content "No Articles Created"
+      end
     end
   end
 end
