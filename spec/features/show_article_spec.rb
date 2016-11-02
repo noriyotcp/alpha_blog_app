5b369eba289854fbf6a9eb121b82f9b0aece5bd6
Noriyo Akita <noriyo.akita@gmail.com>
2016-11-02 08:38:51 +0000

    [show_article_spec] Add scenarios for non-signed user and non-owner

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -1,17 +1,47 @@
 require 'rails_helper'
 
 RSpec.feature "ShowArticle", type: :feature do
-  before do
-    login
-    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: @user)
+  let!(:user) { User.create!(email: "john@example.com", password: "password") }
+  let(:other_user) { User.create!(email: "fred@example.com", password: "password") }
+  let!(:article) { Article.create(title: "The first article", body: "Body of 1st article", user: user) }
+
+  scenario 'to non-signed in user, hide the Edit and Delete buttons' do
+    visit "/"
+    click_link article.title
+
+    expect(page).to have_content article.title
+    expect(page).to have_content article.body
+    expect(current_path).to eq article_path(article)
+
+    expect(page).not_to have_link('Edit Article')
+    expect(page).not_to have_link('Delete Article')
+
   end
 
-  scenario "A user shows an article" do
+  scenario 'to non-owner, hide the Edit and Delete buttons' do
+    login_as(other_user)
     visit "/"
-    click_link @article.title
+    click_link article.title
 
-    expect(page).to have_content @article.title
-    expect(page).to have_content @article.body
-    expect(current_path).to eq article_path(@article)
+    expect(page).to have_content article.title
+    expect(page).to have_content article.body
+    expect(current_path).to eq article_path(article)
+
+    expect(page).not_to have_link('Edit Article')
+    expect(page).not_to have_link('Delete Article')
+
+  end
+
+  scenario "A signed in owner sees an article and both the Edit and Delete buttons" do
+    login_as(user)
+    visit "/"
+    click_link article.title
+
+    expect(page).to have_content article.title
+    expect(page).to have_content article.body
+    expect(current_path).to eq article_path(article)
+
+    expect(page).to have_link 'Edit Article'
+    expect(page).to have_link 'Delete Article'
   end
 end
